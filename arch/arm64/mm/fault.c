4b5ce40e97a2d2886ef67c5166cbdc36ea7eab0f
tomascus <arbiter1000@gmail.com>
2018-03-02 15:20:02 +1100

    arm64: import HTC changes
    Change-Id: I8c5b2de0a1101d9c2a90ddfa1d2e46248b014219

--- arch/arm64/mm/fault.c
+++ arch/arm64/mm/fault.c
@@ -45,6 +45,10 @@
 
 #include <trace/events/exception.h>
 
+#if defined(CONFIG_HTC_DEBUG_RTB)
+#include <linux/msm_rtb.h>
+#endif
+
 static const char *fault_name(unsigned int esr);
 
 #ifdef CONFIG_KPROBES
@@ -171,12 +175,26 @@ static bool is_el1_instruction_abort(unsigned int esr)
 static void __do_kernel_fault(struct mm_struct *mm, unsigned long addr,
 			      unsigned int esr, struct pt_regs *regs)
 {
+#if defined(CONFIG_HTC_DEBUG_RTB)
+	static int enable_logk_die = 1;
+#endif
 	/*
 	 * Are we prepared to handle this kernel fault?
 	 * We are almost certainly not prepared to handle instruction faults.
 	 */
 	if (!is_el1_instruction_abort(esr) && fixup_exception(regs))
 		return;
+
+#if defined(CONFIG_HTC_DEBUG_RTB)
+	if (enable_logk_die) {
+		uncached_logk(LOGK_DIE, (void *)regs->pc);
+		uncached_logk(LOGK_DIE, (void *)regs->regs[30]);
+		uncached_logk(LOGK_DIE, (void *)addr);
+		/* Disable RTB here to avoid weird recursive spinlock/printk behaviors */
+		msm_rtb_disable();
+		enable_logk_die = 0;
+	}
+#endif
 
 	/*
 	 * No handler, we'll have to terminate things with extreme prejudice.
