9757b398a583d53acd25ee919ea569783dbeac56
tomascus <arbiter1000@gmail.com>
2018-03-09 16:06:31 +1100

    tty: import HTC changes
    Change-Id: I191057d541026aed843ad3bc61211fd900c398bf

--- drivers/tty/sysrq.c
+++ drivers/tty/sysrq.c
@@ -51,6 +51,7 @@
 #include <asm/ptrace.h>
 #include <asm/irq_regs.h>
 
+atomic_t em_remount = ATOMIC_INIT(0);
 /* Whether we react on sysrq keys or just ignore them */
 static int __read_mostly sysrq_enabled = CONFIG_MAGIC_SYSRQ_DEFAULT_ENABLE;
 static bool __read_mostly sysrq_always_enabled;
@@ -188,6 +189,7 @@ static struct sysrq_key_op sysrq_show_timers_op = {
 
 static void sysrq_handle_mountro(int key)
 {
+	atomic_set(&em_remount, 1);
 	emergency_remount();
 }
 static struct sysrq_key_op sysrq_mountro_op = {
@@ -296,6 +298,8 @@ static struct sysrq_key_op sysrq_showstate_op = {
 static void sysrq_handle_showstate_blocked(int key)
 {
 	show_state_filter(TASK_UNINTERRUPTIBLE);
+	pr_info("### Show All Tasks in System Server ###\n");
+	show_thread_group_state_filter("system_server", 0);
 }
 static struct sysrq_key_op sysrq_showstate_blocked_op = {
 	.handler	= sysrq_handle_showstate_blocked,
@@ -543,6 +547,8 @@ void __handle_sysrq(int key, bool check_mask)
 	 */
 	orig_log_level = console_loglevel;
 	console_loglevel = CONSOLE_LOGLEVEL_DEFAULT;
+	pr_info("%s (%d:%d) triggered SysRq\n",
+			current->comm, current->tgid, current->pid);
 	pr_info("SysRq : ");
 
         op_p = __sysrq_get_key_op(key);
