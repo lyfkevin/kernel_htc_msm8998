663087212b03f3fe438c4f3eee35eb1e864d7e73
tomascus <arbiter1000@gmail.com>
2018-03-09 15:59:03 +1100

    md: import HTC changes
    Change-Id: Ia71107f1082ef4175155278bd6ad358286c0acc6

--- drivers/md/dm-verity-target.c
+++ drivers/md/dm-verity-target.c
@@ -35,9 +35,15 @@
 
 #define DM_VERITY_OPTS_MAX		(2 + DM_VERITY_OPTS_FEC)
 
+extern atomic_t em_remount;
+
 static unsigned dm_verity_prefetch_cluster = DM_VERITY_DEFAULT_PREFETCH_SIZE;
 
 module_param_named(prefetch_cluster, dm_verity_prefetch_cluster, uint, S_IRUGO | S_IWUSR);
+
+static int dm_verity_hash_prefetch_min_size = CONFIG_DM_VERITY_HASH_PREFETCH_MIN_SIZE;
+module_param_named(prefetch_min_size, dm_verity_hash_prefetch_min_size, int, S_IRUGO | S_IWUSR);
+
 
 struct dm_verity_prefetch_work {
 	struct work_struct work;
@@ -198,6 +204,9 @@ static int verity_handle_err(struct dm_verity *v, enum verity_block_type type,
 	char *envp[] = { verity_env, NULL };
 	const char *type_str = "";
 	struct mapped_device *md = dm_table_get_md(v->ti->table);
+
+	if (atomic_read(&em_remount))
+		return 0;
 
 	/* Corruption should be visible in device status in all modes */
 	v->hash_failed = 1;
@@ -485,7 +494,7 @@ static void verity_end_io(struct bio *bio)
 {
 	struct dm_verity_io *io = bio->bi_private;
 
-	if (bio->bi_error && !verity_fec_is_enabled(io->v)) {
+	if ((bio->bi_error && !verity_fec_is_enabled(io->v)) || atomic_read(&em_remount)) {
 		verity_finish_io(io, bio->bi_error);
 		return;
 	}
@@ -528,12 +537,12 @@ static void verity_prefetch_io(struct work_struct *work)
 				hash_block_end = v->hash_blocks - 1;
 		}
 no_prefetch_cluster:
-		// for emmc, it is more efficient to send bigger read
-		prefetch_size = max((sector_t)CONFIG_DM_VERITY_HASH_PREFETCH_MIN_SIZE,
-			hash_block_end - hash_block_start + 1);
-		if ((hash_block_start + prefetch_size) >= (v->hash_start + v->hash_blocks)) {
-			prefetch_size = hash_block_end - hash_block_start + 1;
-		}
+               // for emmc, it is more efficient to send bigger read
+		prefetch_size = max((sector_t)dm_verity_hash_prefetch_min_size,
+                        hash_block_end - hash_block_start + 1);
+                if ((hash_block_start + prefetch_size) >= (v->hash_start + v->hash_blocks)) {
+                        prefetch_size = hash_block_end - hash_block_start + 1;
+                }
 		dm_bufio_prefetch(v->bufio, hash_block_start,
 				  prefetch_size);
 	}
