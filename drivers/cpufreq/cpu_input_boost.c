40b9f68b9add3e9667780d513d048942995dc708
Yaroslav Furman <yaro330@gmail.com>
2018-12-12 12:01:27 +0300

    cpu_input_boost: avoid boosting big cluster when load is low
    It's really power hungry. Let it idle properly.
    
    [kdrag0n: adapted for general boost]
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -11,6 +11,7 @@
 #include <linux/input.h>
 #include <linux/moduleparam.h>
 #include <linux/slab.h>
+#include "../../kernel/sched/sched.h"
 
 unsigned long last_input_time;
 
@@ -70,13 +71,20 @@ static u32 get_boost_freq(struct boost_drv *b, u32 cpu, u32 state)
 		if (cpumask_test_cpu(cpu, cpu_lp_mask))
 			return general_boost_freq_lp;
 
+	if (cpu_rq(cpu)->nr_running > 1)
 		return general_boost_freq_hp;
+
+	return 0;
+
 	}
 
 	if (cpumask_test_cpu(cpu, cpu_lp_mask))
 		return input_boost_freq_lp;
 
-	return input_boost_freq_hp;
+	if (cpu_rq(cpu)->nr_running > 1)
+		return input_boost_freq_hp;
+
+	return 0;
 }
 
 static u32 get_min_freq(struct boost_drv *b, u32 cpu)
