150d84db5e7921b322532af736ad27ca17a77fd6
lyfkevin <yiuflin@gmail.com>
2019-01-04 22:14:02 +0800

    Revert "scsi: ufs: call single_release to avoid memory leak"
    This reverts commit 587b73fad8fd63906f02bb4978931e8224de460b.

--- drivers/scsi/ufs/ufs-debugfs.c
+++ drivers/scsi/ufs/ufs-debugfs.c
@@ -366,7 +366,6 @@ static const struct file_operations ufsdbg_err_inj_scenario_ops = {
 	.open		= ufsdbg_err_inj_scenario_open,
 	.read		= seq_read,
 	.write		= ufsdbg_err_inj_scenario_write,
-	.release        = single_release,
 };
 
 static int ufsdbg_err_inj_stats_read(struct seq_file *file, void *data)
@@ -408,7 +407,6 @@ static const struct file_operations ufsdbg_err_inj_stats_ops = {
 	.open		= ufsdbg_err_inj_stats_open,
 	.read		= seq_read,
 	.write		= ufsdbg_err_inj_stats_write,
-	.release        = single_release,
 };
 
 static void ufsdbg_setup_fault_injection(struct ufs_hba *hba)
@@ -593,7 +591,6 @@ static const struct file_operations ufsdbg_tag_stats_fops = {
 	.open		= ufsdbg_tag_stats_open,
 	.read		= seq_read,
 	.write		= ufsdbg_tag_stats_write,
-	.release        = single_release,
 };
 
 static int ufsdbg_query_stats_show(struct seq_file *file, void *data)
@@ -665,7 +662,6 @@ static const struct file_operations ufsdbg_query_stats_fops = {
 	.open		= ufsdbg_query_stats_open,
 	.read		= seq_read,
 	.write		= ufsdbg_query_stats_write,
-	.release        = single_release,
 };
 
 static int ufsdbg_err_stats_show(struct seq_file *file, void *data)
@@ -770,7 +766,6 @@ static const struct file_operations ufsdbg_err_stats_fops = {
 	.open		= ufsdbg_err_stats_open,
 	.read		= seq_read,
 	.write		= ufsdbg_err_stats_write,
-	.release        = single_release,
 };
 
 static int ufshcd_init_statistics(struct ufs_hba *hba)
@@ -850,7 +845,6 @@ static int ufsdbg_host_regs_open(struct inode *inode, struct file *file)
 static const struct file_operations ufsdbg_host_regs_fops = {
 	.open		= ufsdbg_host_regs_open,
 	.read		= seq_read,
-	.release        = single_release,
 };
 
 static int ufsdbg_dump_device_desc_show(struct seq_file *file, void *data)
@@ -1024,7 +1018,6 @@ static int ufsdbg_show_hba_open(struct inode *inode, struct file *file)
 static const struct file_operations ufsdbg_show_hba_fops = {
 	.open		= ufsdbg_show_hba_open,
 	.read		= seq_read,
-	.release	= single_release,
 };
 
 static int ufsdbg_dump_device_desc_open(struct inode *inode, struct file *file)
@@ -1036,7 +1029,6 @@ static int ufsdbg_dump_device_desc_open(struct inode *inode, struct file *file)
 static const struct file_operations ufsdbg_dump_device_desc = {
 	.open		= ufsdbg_dump_device_desc_open,
 	.read		= seq_read,
-	.release	= single_release,
 };
 
 static int ufsdbg_dump_health_desc_open(struct inode *inode, struct file *file)
@@ -1048,7 +1040,6 @@ static int ufsdbg_dump_health_desc_open(struct inode *inode, struct file *file)
 static const struct file_operations ufsdbg_dump_health_desc = {
 	.open		= ufsdbg_dump_health_desc_open,
 	.read		= seq_read,
-	.release	= single_release,
 };
 
 static int ufsdbg_power_mode_show(struct seq_file *file, void *data)
@@ -1287,7 +1278,6 @@ static const struct file_operations ufsdbg_power_mode_desc = {
 	.open		= ufsdbg_power_mode_open,
 	.read		= seq_read,
 	.write		= ufsdbg_power_mode_write,
-	.release	= single_release,
 };
 
 static ssize_t ufsdbg_force_write(struct file *file,
@@ -1333,7 +1323,6 @@ static const struct file_operations ufsdbg_force_mode = {
 	.open		= ufsdbg_force_open,
 	.read		= seq_read,
 	.write		= ufsdbg_force_write,
-	.release        = single_release,
 };
 
 
@@ -1514,7 +1503,6 @@ static const struct file_operations ufsdbg_req_stats_desc = {
 	.open		= ufsdbg_req_stats_open,
 	.read		= seq_read,
 	.write		= ufsdbg_req_stats_write,
-	.release        = single_release,
 };
 
 
@@ -1563,7 +1551,6 @@ static const struct file_operations ufsdbg_reset_controller = {
 	.open		= ufsdbg_reset_controller_open,
 	.read		= seq_read,
 	.write		= ufsdbg_reset_controller_write,
-	.release        = single_release,
 };
 
 static int ufsdbg_clear_err_state(void *data, u64 val)
