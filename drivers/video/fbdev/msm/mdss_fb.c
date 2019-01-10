0491a322c1ff2fda5ef76e1b87cffa437e5604ca
lyfkevin <yiuflin@gmail.com>
2019-01-11 02:14:44 +0800

    Revert "Revert "msm: mdss: boost only if last interaction was within 5000 ms""
    This reverts commit db14a4f2e25c15b9513fb685c241e616083426fe.

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -5205,7 +5205,11 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = mdss_fb_mode_switch(mfd, dsi_mode);
 		break;
 	case MSMFB_ATOMIC_COMMIT:
-		cpu_general_boost_kick(64);
+		#ifdef CONFIG_CPU_INPUT_BOOST
+			if (time_before(jiffies, last_input_time + msecs_to_jiffies(3000))) {
+				cpu_general_boost_kick(64);
+			}
+		#endif
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 
