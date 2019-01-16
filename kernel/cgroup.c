0fe906fca62d121c7312681b06b7d2c584949806
crian <crian84@gmail.com>
2019-01-16 07:52:29 +0100

    kernel: Add more launchers to boost CPU when becoming top-app
    Boost OnePlus Launcher and Lawnchair v2 (Public CI/Alpha Builds) as well,
    because both support the Android 9 - Pie recents and app switching.
    They will also suffer from the same stuttering as the Pixel Launcher.
    
    Additionally increase the boost duration to 1000 ms.
    
    Signed-off-by: crian <crian84@gmail.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2777,10 +2777,12 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
 		ret = cgroup_attach_task(cgrp, tsk, threadgroup);
 
 	/* Boost CPU to the max for 500 ms when launcher becomes a top app */
-	if (!memcmp(tsk->comm, "s.nexuslauncher", sizeof("s.nexuslauncher")) &&
-		!memcmp(cgrp->kn->name, "top-app", sizeof("top-app")) && !ret) {
-		cpu_input_boost_kick_max(500);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500);
+ 	if ((!memcmp(tsk->comm, "s.nexuslauncher", sizeof("s.nexuslauncher")) ||
+ 	    !memcmp(tsk->comm, "neplus.launcher", sizeof("neplus.launcher")) ||
+ 	    !memcmp(tsk->comm, "pe.lawnchair.ci", sizeof("pe.lawnchair.ci"))) &&
+	    !memcmp(cgrp->kn->name, "top-app", sizeof("top-app")) && !ret) {
+		cpu_input_boost_kick_max(1000);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 1000);
 	}
 
 	put_task_struct(tsk);
